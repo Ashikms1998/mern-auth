d66d86774f4da9ad8ca8b1f9e3d2718e990a98f1
Ashikms1998 <142452260+Ashikms1998@users.noreply.github.com>
2024-06-05 18:39:38 +0530

    created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,13 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    {/* header */}
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -16,6 +19,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
+      
     </BrowserRouter>
   );
 }
